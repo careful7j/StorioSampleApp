4b7f5f8929b9ab846c4cffac99d523ade7622e75
c7j <careful7j@null.net>
2016-12-15 23:06:12 +0800

    repo created

---
+++ app/src/main/java/c7j/net/storiosampleapp/MainActivity.java
@@ -0,0 +1,13 @@
+package c7j.net.storiosampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
