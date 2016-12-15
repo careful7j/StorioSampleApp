6b6419cc628ac1a4f0ce05dca80858cf0b34ebcf
c7j <careful7j@null.net>
2016-12-16 01:18:19 +0800

    added database helper class

---
+++ app/src/main/java/c7j/net/storiosampleapp/DbOpenHelper.java
@@ -0,0 +1,26 @@
+package c7j.net.storiosampleapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
+
+import c7j.net.storiosampleapp.tables.TweetsTable;
+
+
+public class DbOpenHelper extends SQLiteOpenHelper {
+
+    public DbOpenHelper(@NonNull Context context) {
+        super(context, "sample_db", null, 1);
+    }
+
+    @Override
+    public void onCreate(@NonNull SQLiteDatabase db) {
+        db.execSQL(TweetsTable.getCreateTableQuery());
+    }
+
+    @Override
+    public void onUpgrade(@NonNull SQLiteDatabase db, int oldVersion, int newVersion) {
+        /* NOT IMPLEMENTED YET */
+    }
+}
