42a49a3e16562e4ad44df71216d07fa74048d664
c7j <careful7j@null.net>
2016-12-17 18:45:51 +0800

    CRUD-operations, no autoincrement

--- app/src/main/java/c7j/net/storiosampleapp/MainActivity.java
+++ app/src/main/java/c7j/net/storiosampleapp/MainActivity.java
@@ -1,9 +1,7 @@
 package c7j.net.storiosampleapp;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
@@ -12,6 +10,8 @@ import com.pushtorefresh.storio.StorIOException;
 import com.pushtorefresh.storio.sqlite.StorIOSQLite;
 import com.pushtorefresh.storio.sqlite.impl.DefaultStorIOSQLite;
 import com.pushtorefresh.storio.sqlite.operations.put.PutResults;
+import com.pushtorefresh.storio.sqlite.queries.DeleteQuery;
+import com.pushtorefresh.storio.sqlite.queries.Query;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +21,8 @@ import c7j.net.storiosampleapp.tables.TweetsTable;
 import c7j.net.storiosampleapp.ui.TweetsAdapter;
 
 import c7j.net.storiosampleapp.model.TweetSQLiteTypeMapping;
+
+import static c7j.net.storiosampleapp.tables.TweetsTable.QUERY_ALL;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -46,22 +48,26 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(tweetsAdapter);
 
+        deleteAll();
         addTweets();
+        updateQuery();
+//        queryWithWhere();
+//        deleteWhere();
     }
 
 
-    void addTweets() {
+    private void addTweets() {
         final List<Tweet> tweets = new ArrayList<Tweet>();
 
-        tweets.add(Tweet.newTweet("artem_zin", "Checkout StorIO — modern API for SQLiteDatabase & ContentResolver"));
-        tweets.add(Tweet.newTweet("HackerNews", "It's revolution! Dolphins can write news on HackerNews with our new app!"));
-        tweets.add(Tweet.newTweet("AndroidDevReddit", "Awesome library — StorIO"));
-        tweets.add(Tweet.newTweet("Facebook", "Facebook community in Twitter is more popular than Facebook community in Facebook and Instagram!"));
-        tweets.add(Tweet.newTweet("Google", "Android be together not the same: AOSP, AOSP + Google Apps, Samsung Android"));
-        tweets.add(Tweet.newTweet("Reddit", "Now we can send funny gifs directly into your brain via Oculus Rift app!"));
-        tweets.add(Tweet.newTweet("ElonMusk", "Tesla Model S OTA update with Android Auto 7.2, fixes for memory leaks"));
-        tweets.add(Tweet.newTweet("AndroidWeekly", "Special issue #1: StorIO — forget about SQLiteDatabase, ContentResolver APIs, ORMs suck!"));
-        tweets.add(Tweet.newTweet("Apple", "Yosemite update: fixes for Wifi issues, yosemite-wifi-patch#142"));
+        tweets.add(Tweet.newTweet( 1L, "artem_zin", "Checkout StorIO — modern API for SQLiteDatabase & ContentResolver"));
+        tweets.add(Tweet.newTweet( 2L, "HackerNews", "It's revolution! Dolphins can write news on HackerNews with our new app!"));
+        tweets.add(Tweet.newTweet( 3L, "AndroidDevReddit", "Awesome library — StorIO"));
+        tweets.add(Tweet.newTweet( 4L, "Facebook", "Facebook community in Twitter is more popular than Facebook community in Facebook and Instagram!"));
+        tweets.add(Tweet.newTweet( 5L, "Google", "Android be together not the same: AOSP, AOSP + Google Apps, Samsung Android"));
+        tweets.add(Tweet.newTweet( 6L, "Reddit", "Now we can send funny gifs directly into your brain via Oculus Rift app!"));
+        tweets.add(Tweet.newTweet( 7L, "ElonMusk", "Tesla Model S OTA update with Android Auto 7.2, fixes for memory leaks"));
+        tweets.add(Tweet.newTweet( 8L, "AndroidWeekly", "Special issue #1: StorIO — forget about SQLiteDatabase, ContentResolver APIs, ORMs suck!"));
+        tweets.add(Tweet.newTweet( 9L, "Apple", "Yosemite update: fixes for Wifi issues, yosemite-wifi-patch#142"));
 
         try {
             PutResults<Tweet> results = storIOSQLite
@@ -75,15 +81,118 @@ public class MainActivity extends AppCompatActivity {
             List<Tweet> receivedTweets = storIOSQLite
                     .get()
                     .listOfObjects(Tweet.class)
-                    .withQuery(TweetsTable.QUERY_ALL)
+                    .withQuery(QUERY_ALL)
                     .prepare()
                     .executeAsBlocking();
 
             Toast.makeText(this, getResources().getQuantityString(R.plurals.tweets_loaded, receivedTweets.size()), Toast.LENGTH_LONG).show();
 
-            tweetsAdapter.setTweets(receivedTweets);
+//            tweetsAdapter.setTweets(receivedTweets);
         } catch (StorIOException e) {
             Toast.makeText(this, R.string.tweets_add_error_toast, Toast.LENGTH_LONG).show();
         }
     }
+
+
+
+    private void queryWithWhere() {
+
+        Query QUERY_WITHWHERE = Query.builder()
+                .table(TweetsTable.TABLE)
+                .where(TweetsTable.TABLE + "." + TweetsTable.COLUMN_AUTHOR + " = ?")
+                .whereArgs("Apple")
+                .build();
+
+        List<Tweet> receivedTweets = storIOSQLite
+                .get()
+                .listOfObjects(Tweet.class)
+                .withQuery(QUERY_WITHWHERE)
+                .prepare()
+                .executeAsBlocking();
+
+        Toast.makeText(this, getResources().getQuantityString(R.plurals.tweets_loaded, receivedTweets.size()), Toast.LENGTH_LONG).show();
+
+        tweetsAdapter.setTweets(receivedTweets);
+    }
+
+
+    private void deleteAll() {
+        DeleteQuery DELETEQUERY_ALL = DeleteQuery.builder()
+                .table(TweetsTable.TABLE)
+                .build();
+
+        storIOSQLite.delete()
+                .byQuery(DELETEQUERY_ALL)
+                .prepare()
+                .executeAsBlocking();
+    }
+
+    private void deleteWhere() {
+
+        DeleteQuery DELETEQUERY_WITHWHERE = DeleteQuery.builder()
+                .table(TweetsTable.TABLE)
+                .where(TweetsTable.TABLE + "." + TweetsTable.COLUMN_AUTHOR + " = ?")
+                .whereArgs("Apple")
+                .build();
+
+        storIOSQLite.delete()
+                .byQuery(DELETEQUERY_WITHWHERE)
+                .prepare()
+                .executeAsBlocking();
+
+        List<Tweet> receivedTweets = storIOSQLite
+                .get()
+                .listOfObjects(Tweet.class)
+                .withQuery(QUERY_ALL)
+                .prepare()
+                .executeAsBlocking();
+
+        Toast.makeText(this, getResources().getQuantityString(
+                R.plurals.tweets_loaded, receivedTweets.size()), Toast.LENGTH_LONG).show();
+
+        tweetsAdapter.setTweets(receivedTweets);
+
+    }
+
+    private void updateQuery() {
+        Query QUERY_TWEETS_TO_UPDATE = Query.builder()
+                .table(TweetsTable.TABLE)
+                .where(TweetsTable.TABLE + "." + TweetsTable.COLUMN_ID + " < ?")
+                .whereArgs("5")
+                .build();
+
+        List<Tweet> receivedTweets = storIOSQLite
+                .get()
+                .listOfObjects(Tweet.class)
+                .withQuery(QUERY_TWEETS_TO_UPDATE)
+                .prepare()
+                .executeAsBlocking();
+
+        List<Tweet> receivedTweetsUpdated = new ArrayList<>();
+
+        for ( Tweet tweet : receivedTweets ) {
+            receivedTweetsUpdated.add(Tweet.newTweet(
+                    tweet.id(), tweet.author(), "modified content"));
+        }
+
+        storIOSQLite
+                .put()
+                .objects(receivedTweetsUpdated)
+                .prepare()
+                .executeAsBlocking();
+
+        List<Tweet> receivedTweetsRead = storIOSQLite
+                .get()
+                .listOfObjects(Tweet.class)
+                .withQuery(QUERY_ALL)
+                .prepare()
+                .executeAsBlocking();
+
+        Toast.makeText(this, getResources().getQuantityString(
+                R.plurals.tweets_loaded, receivedTweetsRead.size()), Toast.LENGTH_LONG).show();
+
+        tweetsAdapter.setTweets(receivedTweetsRead);
+
+    }
+
 }
